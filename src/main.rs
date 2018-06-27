1d34ec216cc6bc16a7ab18fdeab470dc68c742db
Seth J. Morabito <web@loomcom.com>
2018-06-27 17:49:46 +0000

    Fix comment

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,21 @@
 /// Copyright 2017, Seth J. Morabito <web@loomcom.com>
 ///
 /// This file is part of the Symbolics Microcode Explorer.
-/// 
-/// Foobar is free software: you can redistribute it and/or modify
-/// it under the terms of the GNU General Public License as published by
-/// the Free Software Foundation, either version 3 of the License, or
-/// (at your option) any later version.
 ///
-/// Foobar is distributed in the hope that it will be useful,
-/// but WITHOUT ANY WARRANTY; without even the implied warranty of
-/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-/// GNU General Public License for more details.
+/// The Symbolics Microde Explorer is free software: you can
+/// redistribute it and/or modify it under the terms of the GNU
+/// General Public License as published by the Free Software
+/// Foundation, either version 3 of the License, or (at your option)
+/// any later version.
+///
+/// The Symbolics Microde Explorer is distributed in the hope that it
+/// will be useful, but WITHOUT ANY WARRANTY; without even the implied
+/// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+/// See the GNU General Public License for more details.
 ///
 /// You should have received a copy of the GNU General Public License
-/// along with Foobar.  If not, see <https://www.gnu.org/licenses/>./
+/// along with The Symbolics Microde Explorer.  If not, see
+/// <https://www.gnu.org/licenses/>.
 ///
 
 extern crate clap;
