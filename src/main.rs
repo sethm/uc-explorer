b610114879a4f2975f5ab055e5877bf2140a4187
Seth Morabito <web@loomcom.com>
2018-06-27 20:15:56 -0700

    Typo fixes

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@
 ///
 /// This file is part of the Symbolics Microcode Explorer.
 ///
-/// The Symbolics Microde Explorer is free software: you can
+/// The Symbolics Microcode Explorer is free software: you can
 /// redistribute it and/or modify it under the terms of the GNU
 /// General Public License as published by the Free Software
 /// Foundation, either version 3 of the License, or (at your option)
 /// any later version.
 ///
-/// The Symbolics Microde Explorer is distributed in the hope that it
+/// The Symbolics Microcode Explorer is distributed in the hope that it
 /// will be useful, but WITHOUT ANY WARRANTY; without even the implied
 /// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 /// See the GNU General Public License for more details.
